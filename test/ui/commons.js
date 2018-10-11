d2c714dd0e6ccbe4c2f6254ca4e5b4e68611ca10
Viktor Kovtun <viktor.kovtun@tib.eu>
2018-10-11 13:35:54 +0200

    Moved driver creation to a separate function in the commons.js file.

--- test/ui/commons.js
+++ test/ui/commons.js
@@ -1,6 +1,23 @@
 const {By, until} = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
 
 module.exports = {
+
+    getWebDriver: function () {
+        // email: "dazuyopuho@bit2tube.com"
+        // name: "tester-tib"
+        // pass: "slidewiki2test"
+
+        //    const userName = 'vkovtun';
+        //    const key = 'fd33af29-4be5-46d4-8b2b-d96b9c9da4a1';
+        //    const url = 'http://' + userName + ':' + key + '@ondemand.saucelabs.com:80/wd/hub';
+        //    const driver = new webdriver.Builder().forBrowser('firefox').usingServer(url).setFirefoxOptions(firefox).build();
+
+        return new webdriver.Builder().forBrowser('firefox').usingServer('http://localhost:4444/wd/hub')
+                .setFirefoxOptions(firefox).build();
+    },
 
     findElement: function (driver, locator) {
         return driver.findElement(locator);
@@ -14,7 +31,10 @@ module.exports = {
         return driver.wait(until.elementIsVisible(element), 10000);
     },
 
-    login: async function (driver, loginEmail, loginPassword) {
+    login: async function (driver) {
+        const loginEmail = 'dazuyopuho@bit2tube.com';
+        const loginPassword = 'slidewiki2test';
+
         await driver.get('http://localhost:3000/');
         await driver.findElement(By.xpath('/html/body'));
         await driver.findElement(By.xpath('//button/span[normalize-space(text())="Sign In"]')).click();
--- test/ui/deck_questions.js
+++ test/ui/deck_questions.js
@@ -1,29 +1,12 @@
 const {By, until} = require('selenium-webdriver');
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const firefox = require('selenium-webdriver/firefox');
+
 const commons = require('./commons');
 
-
-
 (async function deckQuestions() {
-    // email: "dazuyopuho@bit2tube.com"
-    // name: "tester-tib"
-    // pass: "slidewiki2test"
-
-    const loginEmail = 'dazuyopuho@bit2tube.com';
-    const loginPassword = 'slidewiki2test';
-
-//    const userName = 'vkovtun';
-//    const key = 'fd33af29-4be5-46d4-8b2b-d96b9c9da4a1';
-//    const url = 'http://' + userName + ':' + key + '@ondemand.saucelabs.com:80/wd/hub';
-//    const driver = new webdriver.Builder().forBrowser('firefox').usingServer(url).setFirefoxOptions(firefox).build();
-
-    const driver = new webdriver.Builder().forBrowser('firefox').usingServer('http://localhost:4444/wd/hub')
-            .setFirefoxOptions(firefox).build();
+    const driver = commons.getWebDriver();
 
     try {
-        await commons.login(driver, loginEmail, loginPassword);
+        await commons.login(driver);
 
         await commons.findElement(driver, By.id('slideWikiLogo'));
         await commons.waitForElement(driver, By.id('downIcon')).click();
